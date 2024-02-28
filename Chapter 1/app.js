c410d1c3f3151bd32aac43ef1c50ab0dfba5088e
ABDUL SAMAD <samad.asif9@gmail.com>
2024-02-28 20:08:49 +0500

    Add files via upload

---
+++ Chapter 1/app.js
@@ -0,0 +1,6 @@
+alert("Hello,World!")
+alert("Error!Please Enter a Valid Password.")
+alert("Welcome to JS Land....\n Happy Coding!")
+alert("Welcome to JS Land")
+alert("Happy Coding!")
+console.log("Hello...I can run JS through my web browser console")

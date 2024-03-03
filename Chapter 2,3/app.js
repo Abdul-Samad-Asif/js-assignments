a9b585b645038e46b4a33487761ac074c19cb1e8
ABDUL SAMAD <samad.asif9@gmail.com>
2024-03-04 01:21:26 +0500

    Add files via upload

---
+++ Chapter 2,3/app.js
@@ -0,0 +1,39 @@
+// variable for string 
+
+
+// var username;
+// var myName;
+// myName = 'Abdul Samad';
+// var message = 'Hello World!';
+// alert (message);
+// var name = prompt ("Enter Your Name");
+// var age = prompt ("Enter Your Age",'18');
+// var course = prompt ("Enter Your Course",'Web And Mob App Development');
+// alert (name);
+// alert (age);
+// alert (course);
+// var pizza = "Pizza\nPizz\nPiz\nPi\nP";
+// alert (pizza);
+// var email = prompt ("My Email is","example@example.com");
+// alert (email);
+// var book = "A smarter way to learn JavaScript.";
+// alert (book);
+// document.write ("Yay!I Can Write HTML Content Through JS<br>");
+// let exclusive = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert (exclusive);
+// document.write (exclusive);
+
+
+// variable for numbers 
+
+
+// var age = prompt ("Enter Your Age",'18');
+// alert (age);
+// var visit = "You have Visited this page 14 times"
+// alert (visit)
+// var birth = "My Birth Year is 2000"
+// document.write (birth)
+// var yourname = prompt ("Enter You Name")
+// var product = prompt ("Enter Product You Want to Buy")
+// var quantity = prompt ("Quantity you want to buy")
+// document.write (yourname , "ordered" , quantity , product , "on XYZ Clothing Store")
